d2716a1b020c4b99abc0293ca707975ff1e89cb4
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-22 23:23:53 +0530

    Saw the sort method used on arrays in js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-let arr = [, , ,]; //3
-let arr2 = [1, 2, 3, 4]; //4
-let arr3 = [[1, 2, 3], [4, 5, 6]]; //2
+// let arr = [, , ,]; //3
+// let arr2 = [1, 2, 3, 4]; //4
+// let arr3 = [[1, 2, 3], [4, 5, 6]]; //2
 
-console.log(arr.length);
-console.log(arr2.length);
-console.log(arr3.length);
+// console.log(arr.length);
+// console.log(arr2.length);
+// console.log(arr3.length);
+
+
+let arr = [1, 18, 9, 81, 98, 124];
+
+
+arr.sort((a, b) => a - b);
+
+
+// for (let i = 0; i < arr.length; i++){
+//     for (let j = i + 1; j < arr.length; j++){
+//         if (arr[j] < arr[i]) {
+//             let temp = arr[j];
+//             arr[j] = arr[i];
+//             arr[i] = temp;
+//         }
+//     }
+// }
+
+
+
+console.log(arr);

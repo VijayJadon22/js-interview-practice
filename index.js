f178425b76097d208e40740abcee1755ff03fb71
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-24 19:36:38 +0530

    Implemnted what are the ways to empty an array

--- index.js
+++ index.js
@@ -103,3 +103,29 @@
 // let c = a.splice(1, 1);
 // console.log(c); //c=[2] as splice returns the deleted items in new array or empty array
 // console.log(a); //a=[1,3,4,5] 2 is not included slice makes changes in the original array splice is destructive
+
+
+
+
+//**** */
+// what are the ways to empty an array
+// let a = [1, 2, 3, 4, 5];
+
+//1 method by using splice from 0 inde then removing all elements
+// a.splice(0, 5);
+// console.log(a);
+
+//2method  assign to new array
+// a = [];
+// console.log(a);
+
+//3method equate length to 0
+// a.length = 0;
+// console.log(a);
+
+//4method using loop
+// while (a.length) {
+//     a.pop();
+// }
+// console.log(a);
+

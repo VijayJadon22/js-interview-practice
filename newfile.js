cdb775714fd02d73861fb455c791a593eb0d85ef
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-08-20 23:03:18 +0530

    Update newfile.js saw a function

--- newfile.js
+++ newfile.js
@@ -1,4 +1,16 @@
-let c=2;
-let b="2";
-console.log(c==b);
-console.log(c===b);
+// let c=2;
+// let b="2";
+// console.log(c==b);
+// console.log(c===b);
+
+
+for (let i = 0; i < arr.length; i++){
+    for (let j = i + 1; j < arr.length; j++){
+        if (arr[j] < arr[i]) {
+            let temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+console.log(arr);

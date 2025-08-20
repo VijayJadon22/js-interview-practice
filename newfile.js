a122dbe364257bd11be1708bce2fc58f36af8459
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-08-20 23:04:25 +0530

    Update newfile.js commented the new code

--- newfile.js
+++ newfile.js
@@ -19,25 +19,25 @@
 
 
 
-**** */
-what are the ways to empty an array
-let a = [1, 2, 3, 4, 5];
+// **** */
+// what are the ways to empty an array
+// let a = [1, 2, 3, 4, 5];
 
-1 method by using splice from 0 inde then removing all elements
-a.splice(0, 5);
-console.log(a);
+// 1 method by using splice from 0 inde then removing all elements
+// a.splice(0, 5);
+// console.log(a);
 
-2method  assign to new array
-a = [];
-console.log(a);
+// 2method  assign to new array
+// a = [];
+// console.log(a);
 
-3method equate length to 0
-a.length = 0;
-console.log(a);
+// 3method equate length to 0
+// a.length = 0;
+// console.log(a);
 
-4method using loop
-while (a.length) {
-    a.pop();
-}
-console.log(a);
+// 4method using loop
+// while (a.length) {
+//     a.pop();
+// }
+// console.log(a);
 

d8aa22026365182ca1682357f991eb5ca6e14d46
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-08-20 23:04:02 +0530

    Update newfile.js and saw emptying an array

--- newfile.js
+++ newfile.js
@@ -4,13 +4,40 @@
 // console.log(c===b);
 
 
-for (let i = 0; i < arr.length; i++){
-    for (let j = i + 1; j < arr.length; j++){
-        if (arr[j] < arr[i]) {
-            let temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
-        }
-    }
+// for (let i = 0; i < arr.length; i++){
+//     for (let j = i + 1; j < arr.length; j++){
+//         if (arr[j] < arr[i]) {
+//             let temp = arr[j];
+//             arr[j] = arr[i];
+//             arr[i] = temp;
+//         }
+//     }
+// }
+// console.log(arr);
+
+
+
+
+
+**** */
+what are the ways to empty an array
+let a = [1, 2, 3, 4, 5];
+
+1 method by using splice from 0 inde then removing all elements
+a.splice(0, 5);
+console.log(a);
+
+2method  assign to new array
+a = [];
+console.log(a);
+
+3method equate length to 0
+a.length = 0;
+console.log(a);
+
+4method using loop
+while (a.length) {
+    a.pop();
 }
-console.log(arr);
+console.log(a);
+
